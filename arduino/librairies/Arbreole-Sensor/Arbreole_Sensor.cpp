50ccd62499b66d42e41a968f0e74895b50f54fed
yannicktian <yannick.tian@gmail.com>
2018-04-20 02:03:24 +0200

    Send good sensor values

--- arduino/librairies/Arbreole-Sensor/Arbreole_Sensor.cpp
+++ arduino/librairies/Arbreole-Sensor/Arbreole_Sensor.cpp
@@ -12,7 +12,7 @@ void Arbreole_Sensor::Begin(){
 	pinMode(m_pinLed, OUTPUT);
     pinMode(m_pinSensor, INPUT);
 
-    Serial.print("Calibrating the PIR Sensor");
+    Serial.println("Calibrating the PIR Sensor");
     for (int i=0; i < m_calibratingTime; i++) {
         Serial.print(".");
         delay(1000);
@@ -33,7 +33,7 @@ void Arbreole_Sensor::Update(){
         digitalWrite(m_pinLed,LOW);
 
         if(m_sensorState==HIGH){
-            Serial.println("Motion ended: <2>");
+            Serial.println("Motion ended: <0>");
             m_sensorState=LOW;
         }
   }
